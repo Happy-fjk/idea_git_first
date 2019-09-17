49ab3d336b4b9c1d975e2951997dd9c845667b17
fjk <fjiangkun@163.com>
2019-09-17 17:43:02 +0800

    First commit all.

---
+++ com.bonc/src/MyBonc.java
@@ -0,0 +1,17 @@
+/**
+ * @description: 测试方法注释
+ * @author: fjk
+ * @date: Created in 2019/9/17 15:41
+ */
+public class MyBonc {
+    
+
+    /**
+     * @Author fjk
+     * @Param [id, num]
+     * @return java.lang.String
+     **/
+    public String getSum(String id,Integer num){
+        return "";
+    }
+}
